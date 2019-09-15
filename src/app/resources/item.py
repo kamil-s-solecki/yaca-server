515cf0c24cf680925d5d1934bbcc635637f75cfd
Kamil Solecki <kamsolecki@gmail.com>
2019-09-15 11:21:00 +0200

    Remove template item resource

--- src/app/resources/item.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Resource
-from app.models.item import ItemModel
-
-
-class ItemResource(Resource):
-    def get(self):
-        items = ItemModel.query.all()
-        return {'items': [i.json() for i in items]}
--- src/app/routes.py
+++ src/app/routes.py
@@ -1,8 +1,5 @@
 from flask import Blueprint
 from flask_restful import Api
-from app.resources.item import ItemResource
 
 blueprint = Blueprint('app', __name__)
 api = Api(blueprint)
-
-api.add_resource(ItemResource, '/items')
