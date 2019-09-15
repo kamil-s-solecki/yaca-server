89e093d9d4e5ff1a8d38ec837c2dcf58f225c209
Kamil Solecki <kamsolecki@gmail.com>
2019-09-15 17:01:33 +0200

    Add colors to runner

--- test/runner.sh
+++ test/runner.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-python *test.py
+
+
+function run() {
+    CLR='\033[1;36m'
+    NC='\033[0m' # No Color
+
+    echo ""
+    echo -e "${CLR}=========================================================="
+    echo -e "${CLR}    >>> Running $1 <<<"
+    echo -e "${CLR}==========================================================${NC}"
+    echo ""
+    python $1
+}
+
+export -f run
+
+find . -iname '*_test.py' -exec bash -c 'run {}' \;
